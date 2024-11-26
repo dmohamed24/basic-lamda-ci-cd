1b24ba10fc5a8bc394240ff3d222febc02869d4c
Dahir Mohamed <dahir.mohamedd24@gmail.com>
2024-11-26 15:24:50 +0000

    DMO98-2:Update syntax

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda From the Other Sssside!!!!!!!!!')
+        'body': json.dumps('Hello from Lambda From the Other Sssside!!!!!!!!! v2')
     }
